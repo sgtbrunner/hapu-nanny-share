0fb50cb839756510d7edc06e009fd473f59b8994
sgtbrunner <guilherme.brunner@gmail.com>
2021-11-09 18:31:19 -0300

    adds packages to project
    - adds prop-types, styled-components and svg-url-loader libraries to project
    - includes .jsx and .svg extensions on webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 const htmlPlugin = new HtmlWebPackPlugin({
-  template: './src/index.html',
+  template: './public/index.html',
   filename: './index.html',
+  favicon: './public/favicon.ico',
 });
 
 module.exports = {
@@ -17,10 +18,31 @@ module.exports = {
         },
       },
       {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svg-url-loader',
+            options: {
+              limit: 10000,
+            },
+          },
+        ],
+      },
     ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
   },
   plugins: [htmlPlugin],
 };
